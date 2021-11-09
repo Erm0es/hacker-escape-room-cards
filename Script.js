e93818926472fe7c3c679b1ca0221b2c8fffa382
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2021-11-09 14:46:51 +0100

    update

--- Script.js
+++ Script.js
@@ -1,5 +1,23 @@
 console.log("Script is running..");
 
+
+
 function test(){
+    const card = document.querySelector(".pCcard");
     
+    for(let i = 0; i < 14; i++){
+        const array = []; 
+        let newCard = card.cloneNode(true);
+        card.after(newCard);
+        array.push(newCard);  
+    }
 }
+test();
+
+const pCcards = document.querySelectorAll(".pCcard");
+for (let i = 0; i < pCcards.length; i++) {
+  pCcards[i].querySelector("h3").innerText = `new value ${i}`;
+  pCcards[i].querySelector("small").innerText = `new value ${i}`;
+  pCcards[i].querySelector("p").innerText = `new value ${i}`;
+  pCcards[i].querySelector("button").innerText = `new value ${i}`;
+};
