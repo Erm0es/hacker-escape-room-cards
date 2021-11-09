cfced1c2ad55ecee5742b627c60ab8f69332d44c
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2021-11-09 11:57:17 +0100

    Html and css

--- Script.js
+++ Script.js
@@ -1,13 +1,5 @@
 console.log("Script is running..");
 
-var container = document.getElementById("container");
-for(var i=0; 0 <= 15; i++){
-
-    var prop = document.createElement ("div");
-
-    prop.className = "card";
-    prop.id = "card" + i;
-    prop.innerHTML = "ok";
-    container.append(prop);
-
+function test(){
+    
 }
